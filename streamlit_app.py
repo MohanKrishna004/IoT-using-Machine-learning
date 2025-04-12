40ef28caaf5d043f7c126e11e94abfc7d7c85209
MohanKrishna004 <130684960+MohanKrishna004@users.noreply.github.com>
2025-04-12 17:05:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,5 @@ st.info('App for anamoly detection')
 
 with st.expander('Data'):
   st.write('RAW DATA')
-  df = pd.read_csv("/kaggle/input/bot-iot-4ads/UNSW_2018_IoT_Botnet_Full5pc_4.csv", 
-                 low_memory=False)
-  df
+
 
