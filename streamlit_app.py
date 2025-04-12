effb3208129b9b430cfee7cb6aeb11014574d94d
MohanKrishna004 <130684960+MohanKrishna004@users.noreply.github.com>
2025-04-12 17:01:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Smart Home:)')
 
 st.info('App for anamoly detection')
+
+with st.exoander('Data'):
+  st.write('RAW DATA')
+  df = pd.read_csv("/kaggle/input/bot-iot-4ads/UNSW_2018_IoT_Botnet_Full5pc_4.csv", 
+                 low_memory=False)
+  df
